42564e3493d0cf7ad53fd931b5a2f0f09ae6ebac
oyvindhaugen <oyvindet@gmail.com>
2023-12-06 11:48:43 +0100

    git ignore

--- src/day5.rs
+++ src/day5.rs
@@ -11,65 +11,6 @@ pub mod day5 {
 
         (seed, false)
     }
-    pub fn test() {
-        
-        static LINES: &str = include_str!("./inputs/day5/input.txt");
-        // let lines: Vec<&str> = LINES.split("\r\n\r\n").collect();
-        let seeds: Vec<u64> = LINES.lines().collect::<Vec<&str>>()[0]
-            .split(": ")
-            .collect::<Vec<&str>>()[1]
-            .split_ascii_whitespace()
-            .map(|s| s.parse::<u64>().unwrap())
-            .collect();
-        let mut maps: Vec<Vec<Vec<u64>>> = Vec::new(); // all_lines { step { step_line { instructions(u64) } } }
-        let mut lines: Vec<&str> = LINES.split("\r\n\r\n").skip(1).collect::<Vec<&str>>();
-        let llines: Vec<_> = lines
-            .iter()
-            .map(|s| s.split("\r\n").collect::<Vec<&str>>())
-            .collect();
-        dbg!(&lines);
-        // for testing in lines.iter().
-        let mut steps: [&str; 7] = [""; 7];
-        for (i, line) in llines.iter().enumerate() {
-            let mut testing: Vec<Vec<u64>> = Vec::new();
-            for (idx, l) in line.iter().enumerate() {
-                if idx == 0 {
-                    steps[i] = l.split_ascii_whitespace().collect::<Vec<&str>>()[0]
-                } else {
-                    let ll: Vec<u64> = l
-                        .split_ascii_whitespace()
-                        .map(|n| n.parse::<u64>().unwrap())
-                        .collect::<Vec<u64>>();
-                    // maps.push(ll);
-                    testing.push(ll);
-                    // dbg!(ll);
-                }
-            }
-            maps.push(testing);
-        }
-        let mut res: Vec<u64> = Vec::new();
-        for seed in seeds {
-            // let seed = 13;
-            let mut penis = seed;
-            for i in 0..steps.len() {
-                for j in 0..maps[i].len() {
-                    let (n, in_range) = num_for_next(penis, maps[i][j].clone());
-                    penis = n;
-                    if in_range {
-                        break;
-                    }
-                }
-                // print!("\n");
-                // dbg!(penis);
-                // print!("\n");
-            }
-            res.push(penis);
-        }
-
-        dbg!(res.iter().min().unwrap());
-        // dbg!(steps);
-        // dbg!(&maps[0]);
-    }
 
     pub fn solution_1() {
         let start = Instant::now();
@@ -103,19 +44,19 @@ pub mod day5 {
         let mut res: Vec<u64> = Vec::new();
 
         for seed in &seeds {
-            let mut penis = *seed;
+            let mut silly = *seed;
 
             for (_, step) in maps.iter().enumerate() {
                 for j in step {
-                    let (n, in_range) = num_for_next(penis, j.clone());
-                    penis = n;
+                    let (n, in_range) = num_for_next(silly, j.clone());
+                    silly = n;
                     if in_range {
                         break;
                     }
                 }
             }
 
-            res.push(penis);
+            res.push(silly);
         }
 
         // Print or use res as needed
