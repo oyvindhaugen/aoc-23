6f28818569f7ab90933306f2b01c0b4af97366da
oyvindhaugen <95085344+oyvindhaugen@users.noreply.github.com>
2023-12-06 08:44:50 +0100

    fixed some performance issues

--- src/day5.rs
+++ src/day5.rs
@@ -1,70 +1,53 @@
 pub mod day5 {
     pub fn solution_1() {
-        static LINES: &str = include_str!("./inputs/day5/input.txt");
-        let seeds: Vec<u64> = LINES.lines().collect::<Vec<&str>>()[0]
-            .split(": ")
-            .collect::<Vec<&str>>()[1]
-            .split_ascii_whitespace()
-            .map(|s| s.parse::<u64>().unwrap())
-            .collect();
-        let mut maps: Vec<Vec<Vec<u64>>> = Vec::new(); // all_lines { step { step_line { instructions(u64) } } }
-        let mut lines: Vec<&str> = LINES.split("\r\n\r\n").collect::<Vec<&str>>();
-        lines.remove(0);
-        let llines: Vec<_> = lines
-            .iter()
-            .map(|s| s.split("\r\n").collect::<Vec<&str>>())
-            .collect();
-        let mut steps: [&str; 7] = [""; 7];
-        for (i, line) in llines.iter().enumerate() {
-            let mut testing: Vec<Vec<u64>> = Vec::new();
-            for (idx, l) in line.iter().enumerate() {
-                if idx == 0 {
-                    steps[i] = l.split_ascii_whitespace().collect::<Vec<&str>>()[0]
-                } else {
-                    let ll: Vec<u64> = l
-                        .split_ascii_whitespace()
-                        .map(|n| n.parse::<u64>().unwrap())
-                        .collect::<Vec<u64>>();
-                    // maps.push(ll);
-                    testing.push(ll);
-                    // dbg!(ll);
-                }
-            }
-            maps.push(testing);
+    static LINES: &str = include_str!("./inputs/day5/input.txt");
+
+    let seeds: Vec<u64> = LINES.lines().next().unwrap().split(": ").last().unwrap()
+        .split_whitespace()
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+
+    let mut maps: Vec<Vec<Vec<u64>>> = Vec::new();
+
+    for group in LINES.split("\r\n\r\n").skip(1) {
+        let mut testing: Vec<Vec<u64>> = Vec::new();
+        for line in group.lines() {
+            let parts: Vec<u64> = line.split_whitespace().map(|n| n.parse::<u64>().unwrap()).collect();
+            testing.push(parts);
         }
-        let mut res: Vec<u64> = Vec::new();
-        for seed in seeds {
-            let mut curr = seed;
-            for i in 0..steps.len() {
-                for j in 0..maps[i].len() {
-                    let (n, in_range) = num_for_next(curr, maps[i][j].clone());
-                    curr = n;
-                    if in_range {
-                        break;
-                    }
-                }
-            }
-            res.push(curr);
-        }
-        
-        dbg!(res.iter().min().unwrap());
+        maps.push(testing);
     }
 
-    fn num_for_next(seed: u64, step_line: Vec<u64>) -> (u64, bool) {
-        // dbg!(&seed);
-        // dbg!(&step_line);
-        // println!("{:#?} - {:#?}", &step_line[1], &step_line[1] + step_line[2] - 1);
-        if (step_line[1]..step_line[1] + step_line[2]).contains(&seed) {
-            for i in step_line[1]..step_line[1] + step_line[2] {
-                // dbg!(i, i - step_line[1], step_line[0] + (i - step_line[1]));
-                if seed == step_line[1] + (i - step_line[1]) {
-                    // dbg!("please?");
-                    return (step_line[0] + (i - step_line[1]), true)
+    let mut res: Vec<u64> = Vec::new();
+
+    for seed in &seeds {
+        let mut curr = *seed;
+        for (i, step) in maps.iter().enumerate() {
+            for step_line in step {
+                let (n, in_range) = num_for_next(curr, step_line.clone());
+                curr = n;
+                if in_range {
+                    break;
                 }
             }
         }
-        (seed, false)
+        res.push(curr);
     }
+
+    dbg!(res.iter().min());
+}
+
+fn num_for_next(seed: u64, step_line: Vec<u64>) -> (u64, bool) {
+    if (step_line[1]..step_line[1] + step_line[2]).contains(&seed) {
+        for i in step_line[1]..step_line[1] + step_line[2] {
+            if seed == step_line[1] + (i - step_line[1]) {
+                return (step_line[0] + (i - step_line[1]), true);
+            }
+        }
+    }
+    (seed, false)
+}
+
 
     pub fn solution_2() {
         let lines = include_str!("./inputs/day5/input.txt");
